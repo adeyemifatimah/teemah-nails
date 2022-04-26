8e71c007c5a493b25662fe183d0c6555be83ec62
abisoye <beesoye111@gmail.com>
2022-04-26 11:34:38 +0100

    creating an output for the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-$(document).ready(function() {
-    (#tech)
+$(document).ready(function () {
+    $("#tech").submit(function (event) {
+        event.preventDefault();
+
+        const name = $("#name").val();
+        const date = $("date").val();
+        const time = $("time").val();
+        const time2 = $("time2").val();
+        const flavour = $("input:radio[name#flavor]:checked").val();
+        $("#output")
+
+
+        // $("#output").append("Your appointment" + name + "br/>");
+        // $("#output").append("Has been confirmed for" + date + "br/>")
+        // $("#output").append("Which is expected to start for" + "br/>")
+        // $("#output").append("And is expected to and end at" + "br/>")
+        // $("#output").append("Your prefered choice of service is" + "br/>")
+    });
+});
+
