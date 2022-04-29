20986efdc32f14c25b7dc594f2258f44c972518b
abisoye <beesoye111@gmail.com>
2022-04-29 12:49:56 +0100

    added image file

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,27 @@ $(document).ready(function () {
         event.preventDefault();
 
         const name = $("#name").val();
-        const date = $("date").val();
-        const time = $("time").val();
-        const time2 = $("time2").val();
-        const flavour = $("input:radio[name#flavor]:checked").val();
-        $("#output")
+        const date = $("#date").val();
+        const time = $("#time").val();
+        const time2 = $("#time2").val();
+        const nail = $("#nail").val();
 
 
-        // $("#output").append("Your appointment" + name + "br/>");
-        // $("#output").append("Has been confirmed for" + date + "br/>")
-        // $("#output").append("Which is expected to start for" + "br/>")
-        // $("#output").append("And is expected to and end at" + "br/>")
-        // $("#output").append("Your prefered choice of service is" + "br/>")
+
+
+
+        $("#output").append("Your appointment" + name +"<br>");
+        $("#output").append("Has been confirmed for" + date +"<br>" )
+        $("#output").append("Which is expected to start for" + time +"<br>")
+        $("#output").append("And is expected to and end at" + time2 +"<br>")
+        $("#output").append("Your prefered choice of service is" + nail+"<br>")
+
+
+        alert("Your nail appointment has been sucessfully booked " + name)
+        alert("The confirmed date is " + date)
+        alert("Your preferd choice of service is " + nail)
+        alert("Your nail appointment has been confirmed for " + time)
+        alert("Your appointment comes to an end at " + time2)
     });
 });
 
